0fc38ced97677d2223726f1e397c213a3934cf2b
Harshita Chhangani <harshitachhangani05@gmail.com>
2022-07-15 22:59:21 +0530

    first commit

--- Hello/urls.py
+++ Hello/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 
+admin.site.site_header = "Harshita Ice Creams Admin"
+admin.site.site_title = "Harshita Ice Creams Admin Portal"
+admin.site.index_title = "Welcome to Harshita Ice Creams"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
--- home/views.py
+++ home/views.py
@@ -1,15 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is Homepage....")
+    context = {
+        "variable1":"Harshita",
+        "variable2":"Chhangani"
+    }
+    #return HttpResponse("This is Homepage....")
+    return render(request, 'index.html', context)
 
 def about(request):
-    return HttpResponse("This is About Page")
+    return render(request, 'about.html')
+    #return HttpResponse("This is About Page")
 
 def services(request):
-    return HttpResponse("This is Services Page")
+    return render(request, 'services.html')
+    #return HttpResponse("This is Services Page")
 
 def contact(request):
-    return HttpResponse("This is Contact Page")
+    return render(request, 'contact.html')
+    #return HttpResponse("This is Contact Page")
     
